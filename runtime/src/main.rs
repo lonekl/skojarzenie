ade0738faf7df314bff5e8e0169fcd02ff82530e
lonekl <yonek@disroot.org>
2024-06-20 19:41:39 +0200

    Simple basic runtime.

---
+++ runtime/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+
+    eprintln!("{}", skojarzenie::separate_text("Człowiek dla samego siebie jest o wiele więcej skomplikowaną i o wiele trudniejszą do wytłumaczenia istotą niż dla innych, bo ma przed sobą cały świat wewnętrzny, dla tych innych niedostrzegalny i tym samym poniekąd nie istniejący. Gubimy się we własnej duszy, nie umiejąc ani określenia, ani nazwy znaleźć dla rozmaitych jej składników, nie wiedząc, że w jej głębi drzemią instynkty zdolne w odpowiedniej chwili nad całym naszym jestestwem zapanować. I czasami jeden fakt, jedno spostrzeżenie w wewnętrznym świecie jak błyskawica oświetla wnętrze naszej własnej duszy, pozwala nam dojrzeć w niej pierwiastki, o których nigdyśmy nie myśleli i istnienia ich nigdy nie przypuszczali.".to_string()).join(" "));
+
+}

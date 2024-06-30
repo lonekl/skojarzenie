47ba3b7776a6d1aed8240cd2f319d087a65e2fa6
lonekl <yonek@disroot.org>
2024-06-30 19:45:38 +0200

    Basics for new basic terminal interface.

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,40 +1,25 @@
-use std::io::{stdin, stdout, Write};
-use skojarzenie::separate_text;
-use skojarzenie::skoj::Skoj;
+use std::path::Path;
+use crate::interface::terminal::{TerminalProject, TerminalProjectReturn};
+
+pub mod interface;
+
+
 
 fn main() {
-    let mut skoj = Skoj::new();
 
-    loop {
-        print!("[You]: ");
-        stdout().flush().unwrap();
+    println!("You can type with your ai. To use commands, enter them after \':\', \":help\" for example.");
 
-        let mut user_input = "".to_string();
-        stdin().read_line(&mut user_input).unwrap();
+    let mut project = TerminalProject::open(Path::new("/"));
 
-        if &user_input == "exit\n" {
-            break
+    'runtime: loop {
+        match project.enter() {
+            TerminalProjectReturn::ReadError(error) => panic!("{error}"),
+            TerminalProjectReturn::Enter(_) => {},
+            TerminalProjectReturn::OpenNew(_) => {},
+            TerminalProjectReturn::Close => {
+                break 'runtime
+            },
         }
-
-        for word in separate_text(user_input) {
-            skoj.give_word(word);
-        }
-
-        print!("[Skoj]: ");
-        for _ in 0..100 {
-            let skoj_word = skoj.get_word().as_str();
-            print!("{skoj_word}");
-
-            if skoj_word == "." {
-                break
-            }
-            if skoj_word != "," {
-                print!(" ");
-            }
-
-        }
-        println!();
-
     }
 
 }
